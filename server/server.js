03f00923e0c0a49fbd8d7e07845ced8c777afaeb
MKS-students <mks.students@hackreactor.com>
2016-03-31 11:42:11 -0500

    Setup basic server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,42 @@
-server.js
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+
+var app = expess();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var port = process.env.PORT || 4000;
+
+app.listen(port);
+console.log("Listening on: ", port)
