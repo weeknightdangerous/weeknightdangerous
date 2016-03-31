5943f87cd6b7e4ffb15143b66e6a9a02d5718544
Christopher Hummel <chrishummelj@gmail.com>
2016-03-31 12:56:12 -0500

    'Serving index'

---
+++ server/config.js
@@ -0,0 +1,9 @@
+
+
+var config = {};
+
+config.CLIENT_ID = "c1a9f0d89b214eaf9bf1007272c94511";
+config.CLIENT_SECRET ="e3b7f57fa5d54507af11ed94cb34712e";
+
+
+module.exports = config
--- server/server.js
+++ server/server.js
@@ -1,29 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var config = require('./config.js');
+var path = require('path');
 
 
-var app = expess();
+var app = express();
 
 
+app.use(express.static(path.join(__dirname,'../client')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+
+console.log(__dirname + "../client")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/", function(req, res){
+	res.render('index')
+})
 
 
 
@@ -36,7 +33,20 @@ var app = expess();
 
 
 
-var port = process.env.PORT || 4000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+var port = process.env.PORT || 3000;
 
 app.listen(port);
 console.log("Listening on: ", port)
