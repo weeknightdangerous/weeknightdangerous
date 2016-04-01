df1cbf964151f12ea7e8b714b9dc32df83980ad4
Richard Boothe <richardjboothe@me.com>
2016-04-01 14:24:37 -0500

    minor mod

--- client/app/trailsList/trailsList.js
+++ client/app/trailsList/trailsList.js
@@ -16,5 +16,5 @@ angular.module('trailApp.trailsList', [])
 	             "description": "If you are looking for a gentle trail that meanders between semi-dense forest and wide open country then this one is for you. In addition, you will enjoy spectacular views of the San Francisco Peaks worthy of bringing along your camera: especially during the fall season when the aspen trees on the mountain side explode into a glow of brilliant gold.<br /><br />If you\\'re lucky you may also get a chance to take a snapshot of the abundant wildlife that roam this area. Mule deer and elk are often seen foraging along the trail corridor, especially during the early morning and early evening hours. Also, keep you ears open for Abert squirrels and Steller\\'s jays that often compete to see who can be the loudest, and don\\'t be alarmed to hear the early morning yelps of coyotes as they bid one another good morning.<br /><br />This trail, which is relatively flat, travels north from Little Elden Springs Horse Camp towards Sunset Crater. It was constructed primarily to accommodate equestrians that visit the campground, but by no means does that restrict hikers and mountain bikers from using the trail. In fact, don\\'t be surprised if you see more hikers and mountain bikers on the trail than horses.",
 	             "rating": 2.50
 	 }
-	];
+	];	
 });
