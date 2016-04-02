1abd42a9baa9b3b2b71f16c83b9b95c1338133db
Connie Yu <nonlogos1@gmail.com>
2016-04-01 19:13:01 -0500

    added angular-animate

--- client/app/myApp.js
+++ client/app/myApp.js
@@ -5,7 +5,8 @@ angular.module('trailApp', [
   'trailApp.profile',
   'trailApp.comment',
   'trailApp.trailsList',
-  'ui.router'
+  'ui.router',
+  'ngAnimate'
   ])
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/home');
@@ -54,16 +55,32 @@ angular.module('trailApp', [
 
       })
 }])
+// .factory('AttachTokens', function ($window) {
+//   // this is an $httpInterceptor
+//   // its job is to stop all out going request
+//   // then look in local storage and find the user's token
+//   // then add it to the header so the server can validate the request
+//   var attach = {
+//     request: function (object) {
+//       var jwt = $window.localStorage.getItem('com.shortly');
+//       if (jwt) {
+//         object.headers['x-access-token'] = jwt;
+//       }
+//       object.headers['Allow-Control-Allow-Origin'] = '*';
+//       return object;
+//     }
+//   };
+//   return attach;
+// })
 
-// .run(function ($rootScope, $location) {
-//   // here inside the run phase of angular, our services and controllers
-//   // have just been registered and our app is ready
-//   // however, we want to make sure the user is authorized
+// .run(function ($rootScope, $location, Auth) {
 //   // we listen for when angular is trying to change routes
 //   // when it does change routes, we then look for the token in localstorage
 //   // and send that token to the server to see if it is a real user or hasn't expired
 //   // if it's not valid, we then redirect back to signin/signup
 //   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
-//     
-//   })
+//     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+//       $location.path('/signin');
+//     }
 //   });
+// });
