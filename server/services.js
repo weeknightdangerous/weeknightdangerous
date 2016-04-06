921e2605a6c711ec54299776b61408c03c1eb9ff
Christopher Hummel <chrishummelj@gmail.com>
2016-04-06 16:05:46 -0500

    chnage db schema for comments, finished comment services, added checkAuth function

--- server/services.js
+++ server/services.js
@@ -1,11 +1,23 @@
- 
-var db = require('./database/dbhelpers')
+ var db = require('./database/dbhelpers')
 
 
-exports.addComment =  function() {};
+exports.addComment =  function(req, res) {
+  dbhepers.addComment(res.locals.userId, req.data.trailID, req.data.comment)
+    .then(function(resp) {
+      res.send(resp)
+    })
 
-exports.checkCookie =  function() {
+};
 
+exports.checkCookie =  function(req) {
+  dbhelpers.findUserBySession(req.cookies.trailrpark)
+    .then(function(resp){
+      if (!!resp[0]) {  
+        res.locals.userId = resp[0]; 
+        return true 
+      }
+      return false
+    })
 };
 
 exports.addFav = function() {};
