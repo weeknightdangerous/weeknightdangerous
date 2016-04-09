b16cfce37ed92947c55944801ce4ac162ec89b8e
Richard Boothe <richardjboothe@me.com>
2016-04-09 14:09:51 -0500

    improve rating directive

--- client/app/rating/rating.js
+++ client/app/rating/rating.js
@@ -1,9 +1,15 @@
-angular.module('trailApp.rating',[])
-.controller('RatingCtrl', function ($scope) {
+angular.module('trailApp.rating', [])
+.controller('RatingCtrl', ['$scope', function ($scope) {
   $scope.rate = 3;
   $scope.max = 5;
   $scope.isReadonly = true;
   $scope.ratingStates = [
     {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'}
   ];
+})
+.directive('rating', function(){
+  return {
+    restric:'AEC'
+    templateUrl: 'rating.html'
+  };
 });
