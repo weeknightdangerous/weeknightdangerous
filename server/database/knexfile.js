1d52ce90fb2cb07b44c5f3e34ff886f53adc29ac
Sam Fitz <abigwonderful@gmail.com>
2016-04-11 12:35:11 -0500

    database connection working

--- server/database/knexfile.js
+++ server/database/knexfile.js
@@ -1,9 +1,13 @@
 var path = require('path')
+
+//direct connection from local postgres to heroku
+//heroku pg:push dev DATABASE_URL --app trailrpark
+
 module.exports = {
 	development: {
 		client: 'postgresql',
 		connection: {
-			database : process.env.DATABASE_URL || 'dev',
+			database : 'decfjf795ca6f2',
       user: 'rotmbgwgdroduf',
       password: 'EDLnPqifIiYE_-RtdaD8LEbAfp',
       host: 'ec2-23-21-255-14.compute-1.amazonaws.com',
@@ -12,3 +16,4 @@ module.exports = {
 		}
 	}
 }
+
