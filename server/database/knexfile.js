f6fbb6eae765a4608322b08eacaf7ceb05821bbb
Sam Fitz <abigwonderful@gmail.com>
2016-04-11 11:48:10 -0500

    updated db connection string

--- server/database/knexfile.js
+++ server/database/knexfile.js
@@ -4,6 +4,11 @@ module.exports = {
 		client: 'postgresql',
 		connection: {
 			database : process.env.DATABASE_URL || 'dev',
+      user: 'rotmbgwgdroduf',
+      password: 'EDLnPqifIiYE_-RtdaD8LEbAfp',
+      host: 'ec2-23-21-255-14.compute-1.amazonaws.com',
+      port: 5432,
+      ssl: true
 		}
 	}
 }
