9099fa464acb7ee54404f035f8b496f990442030
Sam Fitz <abigwonderful@gmail.com>
2016-04-10 22:12:51 -0500

    modal working, but not a directive!

--- client/app/trailProfile/trailProfile.js
+++ client/app/trailProfile/trailProfile.js
@@ -1,7 +1,7 @@
-angular.module('trailApp.profile', [])
+angular.module('trailApp.profile', ['ui.bootstrap'])
 
 
-.controller('profileCtrl', function( showTrails, addFav, imageService, $scope, $state) {
+.controller('profileCtrl', function( $uibModal,showTrails, addFav, imageService, $scope, $state) {
    
   var profile = this;
   
@@ -9,35 +9,61 @@ angular.module('trailApp.profile', [])
   profile.slides= {};
   profile.loading = true;
   profile.showModal = false;
-  profile.toggleModal = function(){
-      profile.showModal = !profile.showModal;
-      console.log('toggled');
+
+  //click image, show modal
+  profile.open = function (slide) {
+    profile.selected = slide
+    $uibModal.open({
+      template: '<my-modal modal="modal">' +
+        '<div class="modal-body">' + 
+          '<img class="img-responsive" src="{{ viewer.slide.image.high_res.url }}">' +
+          '<a class="user-link" target="blank" href="{{viewer.slide.link}}">' +
+            '<img class="img-circle profile gram-user" src="{{viewer.slide.user.profile_pic}}">' +
+          '</a>' +
+        '</div>' +
+      '</my-modal>',
+      controller: 'ModalInstanceCtrl as viewer',
+      resolve: {
+        items: function(){
+          console.log(profile.selected)
+          return profile.selected;
+        }
+      }
+
+    });
   };
 
-    //get trail info from the stored value in showTrails service by using showTrails.getTrail(); 
-    profile.getTrail = function() {
-      profile.data = showTrails.getTrail();
-     };
+  //get trail info from the stored value in showTrails service by using showTrails.getTrail(); 
+  profile.getTrail = function() {
+    profile.data = showTrails.getTrail();
+   };
 
-    profile.addFav = function() {
-      return addFav.postFav()
-        .then(function (result) {
-          console.log('addFavClient result:', result);
-        })
-        .catch(function (err) {
-          console.error('addFavClient error:', err);
-        })
+  profile.addFav = function() {
+    return addFav.postFav()
+      .then(function (result) {
+        console.log('addFavClient result:', result);
+      })
+      .catch(function (err) {
+        console.error('addFavClient error:', err);
+      })
 
-    } 
-    
-    //initialize the trail data
-    profile.getTrail();
+  } 
+  
+  //initialize the trail data
+  profile.getTrail();
 
-    //grab our images
-    imageService.getImages()
-    .then(function(data){
-      profile.slides = data;
-      profile.loading = false;
-    });
+  //grab our images
+  imageService.getImages()
+  .then(function(data){
+    profile.slides = data;
+    profile.loading = false;
+  });
 
+})
+.controller('ModalInstanceCtrl', function ($uibModalInstance, items) {
+
+    var viewer = this;
+
+    //I want to get the vehicle data pass to populate the form myModalContent.html
+    viewer.slide = items;
 })
